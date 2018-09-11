3f33a079e91b02058425ef84bca3d414e6d11026
lotco <kgsin7@gmail.com>
2018-09-11 14:33:19 +0900

    init

---
+++ USSwift/USSwift/New Group/ViewController/USMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  USMainViewController.swift
+//  USSwift
+//
+//  Created by lotco on 2018. 9. 10..
+//  Copyright © 2018년 lotco. All rights reserved.
+//
+
+import UIKit
+
+class USMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NSLog("%@", "Main View Controller did load");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
